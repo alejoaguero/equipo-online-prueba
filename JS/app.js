abd3601eae4ad40da5738a81d8d5b597e1391924
Alejo <alejoleonel45@gmail.com>
2022-03-26 10:53:13 -0300

    pagina de prueba

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,23 @@
-const menuDesple = document.querySelector('#menuDesple')
-const menuBackground = document.querySelector('#menuBack')
-const menu = document.querySelector('#menu')
-
-menuDesple.addEventListener('click',()=>{
-    menu.classList.add('header__menuOn')
+const typed = new Typed('.typed ', {
+    strings: [
+        '<i class="texto-automatico">capacitaciones.</i>',
+        '<i class="texto-automatico">on boarding.</i>',
+        '<i class="texto-automatico">fortalecer vinculos.</i>',
+        '<i class="texto-automatico">una actividad virtual.</i>',
+        '<i class="texto-automatico">fomentar valores de la empresa.</i>',
+        '<i class="texto-automatico">trabajar o detectar habilidades blandas.</i>',
+        '<i class="texto-automatico">solo diversión.</i>'
+    ],
+    stringsElement: '#text-autocomplete', // ID del elemento que contiene cadenas de texto a mostrar.
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    shuffle: false, // Alterar el orden en el que escribe las palabras.
+    backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+    loop: true, // Repetir el array de strings
+    loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+    showCursor: true, // Mostrar cursor palpitanto
+    cursorChar: '|', // Caracter para el cursor
+    contentType: 'html', // 'html' o 'null' para texto sin formato
 })
