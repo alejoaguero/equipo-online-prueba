02f8603858283b9e7c368753fa6658d4736d346b
Emanuel Ferreira <emanuelf88@gmail.com>
2022-04-03 00:20:52 -0300

    Agregando secciones

--- JS/app.js
+++ JS/app.js
@@ -21,3 +21,47 @@ const typed = new Typed('.typed ', {
     cursorChar: '|', // Caracter para el cursor
     contentType: 'html', // 'html' o 'null' para texto sin formato
 })
+
+// inicializar el efecto de scroll
+AOS.init();
+
+// 
+$(document).ready(function(){
+
+  if($('.brands_slider').length)
+  {
+  var brandsSlider = $('.brands_slider');
+  
+  brandsSlider.owlCarousel(
+  {
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:3000,
+  nav:false,
+  dots:false,
+  autoWidth:true,
+  items:8,
+  margin:42
+  });
+  
+  if($('.brands_prev').length)
+  {
+  var prev = $('.brands_prev');
+  prev.on('click', function()
+  {
+  brandsSlider.trigger('prev.owl.carousel');
+  });
+  }
+  
+  if($('.brands_next').length)
+  {
+  var next = $('.brands_next');
+  next.on('click', function()
+  {
+  brandsSlider.trigger('next.owl.carousel');
+  });
+  }
+  }
+  
+  
+  });
